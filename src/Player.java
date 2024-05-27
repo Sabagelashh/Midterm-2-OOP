d404dd45beadd08d42062b633a2a98272dd6fdbd
Sabagelashh <147238067+Sabagelashh@users.noreply.github.com>
2024-05-27 09:57:44 +0400

    Create Player.java

---
+++ src/Player.java
@@ -0,0 +1,20 @@
+// Player.java
+import java.io.Serializable;
+
+public class Player implements Serializable {
+    private String name;
+    private int age;
+
+    public Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
